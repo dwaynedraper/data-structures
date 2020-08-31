dc1b95fbea2423d172503176cd9f7b2a6297700b
Dwayne Draper <deandraper82@gmail.com>
2020-08-31 18:17:38 -0500

    Complete functional stack and queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let end = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    end++;
+    storage[end] = value;
   };
 
   someInstance.pop = function() {
+    let value = storage[end];
+    delete storage[end];
+    end--;
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length
   };
 
   return someInstance;
