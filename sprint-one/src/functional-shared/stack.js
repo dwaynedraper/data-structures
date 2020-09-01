654daa56e89724f9ac429a6c459abfa75b3d464f
Dwayne Draper <deandraper82@gmail.com>
2020-08-31 19:59:29 -0500

    Implemented functional-shared version of stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  let someInstance = {};
+  someInstance.storage = {};
+  someInstance.end = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
 
-
+stackMethods.push = function(value) {
+  this.end++;
+  this.storage[this.end] = value;
+};
+stackMethods.pop = function() {
+  if (this.end === 0) {
+    return 0;
+  }
+  let value = this.storage[this.end];
+  delete this.storage[this.end];
+  this.end--;
+  return value;
+};
+stackMethods.size = function() {
+  return this.end;
+};
+let stack = Stack();
