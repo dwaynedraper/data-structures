b2fe47bba0cdbd479510d826b32637ef7dc8ac80
Dwayne Draper <deandraper82@gmail.com>
2020-09-01 17:13:34 -0500

    Implemented stack.js in es6 style

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,20 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
-
+  push(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  }
+  pop() {
+    let value = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return value;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
