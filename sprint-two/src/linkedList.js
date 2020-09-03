e383010fadf616a8dd1d728eb37beb8c4d4f0bd3
Dwayne Draper <deandraper82@gmail.com>
2020-09-02 21:05:06 -0500

    Updating Linked List with time complexity statements that were missed before. Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -22,10 +22,6 @@ var LinkedList = function() {
 
   list.contains = function(target) {
     let findValue = function(node) {
-      console.log('target: ', target);
-      console.log('node: ', node);
-      console.log('value: ', node.value);
-      console.log('next: ', node.next);
       if (node.value === target) {
         return true;
       } else if (node.next === null) {
@@ -50,5 +46,8 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+* Complexity: What is the time complexity of the above functions?
+The time complexity of addToTail and removeHead are both o(1) because we do not have to do any searching. List.head and list.tail are both kept in memory to add efficiency. It is simply inserting or deleting. If we had to search first, it would become O(n).
+The time complexity of contains is O(n) as we are iterating through each node of the Linked List searching for a specific value. Best case is time complexity O(1) if the first node's value is the target.
+
+*/
