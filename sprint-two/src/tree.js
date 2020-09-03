2719857f8eb97dc409a13afa2d5c5a3233163822
Dwayne Draper <deandraper82@gmail.com>
2020-09-02 22:25:55 -0500

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,19 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  return _.some(this.children, (el) => el.contains(target));
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  The time complexity of addChild is O(1).
+  The time complexity of contains is quadratic time complexity because of the recursive nature of the call
  */
