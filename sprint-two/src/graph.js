2d8caa8899c7b7fe2b7a733d0823fd10bc2602e9
Dwayne <deandraper82@gmail.com>
2020-09-03 23:19:54 -0500

    Implemented graph, still need time complexity

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -5,8 +5,6 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  console.log(this);
-  console.log(this.nodes);
   this.nodes.push({
     'value': node,
     'edges': []
@@ -22,27 +20,63 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   let nodes = this.nodes;
+  let ind = this.findIndex(node);
+  let edges = this.nodes[ind].edges.slice();
+  for (let i = 0; i < edges.length; i++) {
+    this.removeEdge(edges[i], node);
+  }
   _.each(nodes, function(el, i) {
     if (el.value === node) {
       nodes.splice(i, 1);
     }
   });
+
 };
 
+Graph.prototype.findIndex = function(node) {
+  let index;
+  let nodes = this.nodes;
+  _.each(nodes, (el, i) => {
+    if (el.value === node) {
+      index = i;
+    }
+  });
+  return index;
+};
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  let fromIndex = this.findIndex(fromNode);
+  if (this.nodes[fromIndex].edges.includes(toNode)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  let fromIndex = this.findIndex(fromNode);
+  let toIndex = this.findIndex(toNode);
+  this.nodes[fromIndex].edges.push(toNode);
+  this.nodes[toIndex].edges.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  let fromIndex = this.findIndex(fromNode);
+  let toIndex = this.findIndex(toNode);
+  let filtered = this.nodes[fromIndex].edges.filter(el => el === fromNode);
+  this.nodes[fromIndex].edges = filtered;
+  filtered = this.nodes[toIndex].edges.filter(el => el === toNode);
+  this.nodes[toIndex].edges = filtered;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
+  for (let i = 0; i < this.nodes.length; i++) {
+    cb(this.nodes[i].value);
+  }
 };
 
 /*
