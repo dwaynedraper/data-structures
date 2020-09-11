af578e0df871d26ad4f63da06d029106958e75a6
Dwayne Draper <deandraper82@gmail.com>
2020-09-11 18:45:47 -0500

    Complete Hash Table

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,27 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (!this.contains(item)) {
-    this._storage.push(item);
-  }
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
-  return this._storage.indexOf(item) > -1;
+  return item in this._storage;
 };
 
 setPrototype.remove = function(item) {
-  let filtered = this._storage.filter(el => el !== item);
-  this._storage = filtered;
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
-  All three functions have linear time complexity, as all items in the storage array must be checked before running the operation.
+  All three functions have constant time complexity as we're using object lookup for each function.
  */
